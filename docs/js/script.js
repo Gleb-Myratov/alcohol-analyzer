9d4227594efe128f1fc0453dbe7fd7028207037e
Gleb <gleb.myratoff@gmail.com>
2024-11-29 16:22:40 +0300

    Add functionality for cart item removal.

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,4 +1,5 @@
 const modalOverlay = document.getElementById("modal");
+const table = document.querySelector(".table__content");
 function openModal() {
   document.getElementById("modal").classList.add("active");
 }
@@ -7,3 +8,9 @@ function closeModal() {
   if (event.target == modalOverlay)
     document.getElementById("modal").classList.remove("active");
 }
+function deleteRow(numberRow) {
+  document.querySelector(`.table__row[data-row="${numberRow}"]`).remove();
+  table.style.gridTemplateRows = `repeat(${table.children.length}, 1fr)`;
+  // table.classList.add("new-grid-style");
+}
+console.log(document.styleSheets[1]);
