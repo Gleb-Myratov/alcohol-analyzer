9fab073d2d53fdd03abb6a6f0db5cd6d58d807d1
Gleb <gleb.myratoff@gmail.com>
2024-12-03 16:29:04 +0300

    Update HTML and CSS styles.

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,9 @@
 const modalOverlay = document.getElementById("modal");
 const table = document.querySelector(".table__content");
+const deleteRowButtons = document.querySelectorAll(".table__remove-button");
+
+deleteRowButtons[2].addEventListener("click", deleteRow1);
+
 function openModal() {
   document.getElementById("modal").classList.add("active");
 }
@@ -8,9 +12,17 @@ function closeModal() {
   if (event.target == modalOverlay)
     document.getElementById("modal").classList.remove("active");
 }
-function deleteRow(numberRow) {
+/* function deleteRow(numberRow) {
   document.querySelector(`.table__row[data-row="${numberRow}"]`).remove();
   table.style.gridTemplateRows = `repeat(${table.children.length}, 1fr)`;
   // table.classList.add("new-grid-style");
+} */
+const rowInpiuts = document.getElementById("row-inputs");
+
+function test() {
+  console.log(rowInpiuts.querySelector("input").value);
 }
-console.log(document.styleSheets[1]);
+
+function deleteRow1() {
+  console.dir(this);
+}
