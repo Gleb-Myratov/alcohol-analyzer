f40250d543e31bba3260f9f65878ab0a8a4051f3
Gleb <gleb.myratoff@gmail.com>
2024-12-05 16:48:16 +0300

    Add dynamic functionality to the table.

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,6 @@
 const tabel = document.querySelector(".tabel__content");
 
-const removeRowButtons = document.querySelectorAll(".tabel__remove-button");
+const removeRowButtons = document.querySelectorAll(".tabel__remove-button"); // корзина
 const mainAddButton = document.getElementById("main-add-button");
 const createRowButton = document.getElementById("create-row-button");
 
@@ -8,37 +8,60 @@ const modal = document.getElementById("modal");
 const rowInpiuts = document.getElementById("row-inputs");
 const tabelInputs = document.querySelectorAll(".tabel__cell--input");
 
-const tabelTemplate = document.querySelector(".tabel__template");
+const tabelTemplate = document.querySelector(".tabel__template"); // шаблон строки
 
-removeRowButtons.forEach((el, i) => {
-  el.addEventListener("click", () => removeRow(el, i));
-  // el.setAttribute("data-row-remove", `${i}`);
+removeRowButtons.forEach((el) => {
+  el.addEventListener("click", () => {
+    el.parentElement.parentElement.remove();
+  });
 });
-function removeRow(el, i) {
-  el.parentElement.parentElement.remove();
-}
 
+tabelInputs.forEach((el) => {
+  el.value = 111; // временно заполнил инпуты
+});
+
+// модальное окно
 mainAddButton.addEventListener("click", () => {
   modal.classList.add("active");
 });
 
-modal.addEventListener("click", () => {
+modal.addEventListener("click", (event) => {
   if (event.target == modal) modal.classList.remove("active");
 });
 
+//  добавление заполненной строки в таблицу
 createRowButton.addEventListener("click", () => {
-  let emptyInputs = false;
-  tabelInputs.forEach((el) => {
-    if (el.value.length === 0) emptyInputs = true;
+  let rnd = Math.random();
+  let emptyInputs = false; // пустая ячейка
+  tabelInputs.forEach((el, i) => {
+    if (el.value.length === 0) {
+      emptyInputs = true;
+      tabelInputs[i].classList.remove("input-required");
+      tabelInputs[i].classList.add("input-required");
+    } // проверка на пустой инпут
   });
   if (!emptyInputs) {
-    tabel.append(tabelTemplate.content.cloneNode(true));
-    let newRow = tabel.querySelectorAll(".tabel__row");
-    let newRowCells =
-      newRow[newRow.length - 1].querySelectorAll(".tabel__cell");
+    tabel.append(tabelTemplate.content.cloneNode(true)); // клонирование шаблонаи добавление в таблу
+    let tabRows = tabel.querySelectorAll(".tabel__row");
+    let newRow = tabRows[tabRows.length - 1]; // новая строка
+    let newRowList = newRow.querySelectorAll(".tabel__cell"); // список новой строки
+    let newRowDelBtn = newRow.querySelector(".tabel__remove-button"); // кнопка нов строки
+    let newRowLabel = newRow.querySelector(".tabel__checkbox-label"); // лейбл
+    let newRowInput = newRow.querySelector(".tabel__checkbox-button"); // чекбокс
+
     for (let i = 0; i < 5; i++) {
-      newRowCells[i + 1].textContent = tabelInputs[i].value;
+      newRowList[i + 1].textContent = tabelInputs[i].value; // добавление в шаблон из инпутов
     }
+    newRowDelBtn.addEventListener("click", () =>
+      newRowDelBtn.parentElement.parentElement.remove()
+    ); // удаление строки
+
+    newRow.setAttribute("data-row", `${rnd}`);
+    newRowLabel.setAttribute("for", `${rnd}`);
+    newRowInput.setAttribute("id", `${rnd}`);
+    console.log(tabelInputs);
+
+    // tabelInputs.forEach((el) => (el.value = "")); // очистка инпута
     modal.classList.remove("active");
   } else {
     console.log("добавь");
